396885aa7ee43245750f5c2d4e61da334853cb2f
kverhun <k.verhun@gmail.com>
2016-01-03 13:27:08 +0200

    create main widget class

--- src/main.py
+++ src/main.py
@@ -7,14 +7,24 @@ from PyQt5.QtWidgets import QApplication, QWidget
 from google_calendar_access import sync_google_data_with_database
 
 
+class MainWidget(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.InitGUI()
+
+
+    def InitGUI(self):
+        self.resize(250, 150)
+        self.move(300, 300)
+        self.setWindowTitle('Application title')
+
+
 if __name__ == '__main__':
     sync_google_data_with_database()
 
     app = QApplication(sys.argv)
-    w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Application title')
+    w = MainWidget()
     w.show()
 
     sys.exit(app.exec_())
