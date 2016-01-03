ca1eaf4e49283862085dc161d631caf03c8fb441
kverhun <k.verhun@gmail.com>
2016-01-03 14:22:40 +0200

    store added items in table

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,7 +1,7 @@
 __author__ = 'Kostya'
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QTableWidgetItem
 
 from google_calendar_access import sync_google_data_with_database
 
@@ -10,6 +10,7 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.ui = None
+        self.last_task_id = 0
         self.InitGUI()
 
     def InitGUI(self):
@@ -19,4 +20,11 @@ class MainWindow(QWidget):
 
     def OnTaskAdd(self):
         title = self.ui.taskTitleEdit.text()
-        print('New task: title', title)
+        id = self.last_task_id + 1
+        self.last_task_id = id
+        print('New task: title', title, 'id:', id)
+        row_number = self.ui.tasksTableWidget.rowCount()
+        self.ui.tasksTableWidget.insertRow(row_number)
+        self.ui.tasksTableWidget.setItem(row_number, 0, QTableWidgetItem(str(id)))
+        self.ui.tasksTableWidget.setItem(row_number, 1, QTableWidgetItem(title))
+        self.ui.tasksTableWidget.setItem(row_number, 2, QTableWidgetItem(str(0)))
