bd2074981c6bdf25596743f0a49a408313e18d8d
kverhun <k.verhun@gmail.com>
2016-01-03 13:52:58 +0200

    Use qt-designer ui-file. Add sync button to window.

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ __author__ = 'Kostya'
 
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5 import uic
 
 from google_calendar_access import sync_google_data_with_database
 
@@ -11,20 +12,16 @@ class MainWidget(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.ui = None
         self.InitGUI()
 
-
     def InitGUI(self):
-        self.resize(250, 150)
-        self.move(300, 300)
-        self.setWindowTitle('Application title')
+        self.ui = uic.loadUi('gui/MainWindow.ui', self)
+        self.ui.syncButton.clicked.connect(sync_google_data_with_database)
 
 
 if __name__ == '__main__':
-    sync_google_data_with_database()
-
     app = QApplication(sys.argv)
     w = MainWidget()
     w.show()
-
     sys.exit(app.exec_())
